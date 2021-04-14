6b6ec543e83f0aedf4727a9dba1e15cff8229a2f
Vadim <nikitin.vl@phystech.edu>
2021-04-14 08:36:16 +0300

    Написан тест для программы

--- 9_4sem/main.cpp
+++ 9_4sem/main.cpp
@@ -165,29 +165,14 @@ private:
 
 int main(int argc, char** argv)
 {
-	using allocator = boost::interprocess::allocator < char,
-		boost::interprocess::managed_shared_memory::segment_manager >;
+    boost::interprocess::shared_memory_object::remove("shared_memory");
+    std::string user_name;
 
-	using string = boost::interprocess::basic_string < char,
-		std::char_traits < char >, allocator>;
+    std::cout << "Enter your name: ";
 
-	const std::string shared_memory_name = "managed_shared_memory";
+    std::getline(std::cin, user_name);
 
-	boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());
+    Chat(user_name).run();
 
-	boost::interprocess::managed_shared_memory shared_memory(
-		boost::interprocess::open_or_create, shared_memory_name.c_str(), 1024);
-
-	auto s = shared_memory.find_or_construct < string >("String")("Hello",
-		shared_memory.get_segment_manager());
-
-	*s += ", world!";
-
-	std::cout << *s << std::endl;
-
-	boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str());
-
-	system("pause");
-
-	return EXIT_SUCCESS;
+    return 0;
 }
